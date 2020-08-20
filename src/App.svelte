9f2387d0a64270b8e5b254b1394fd348d82227ba
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-08-19 21:41:51 -0700

    added meetingsitem component

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,41 @@
 <script>
-    import Header from './UI/Header.svelte'
+    import Header from './UI/Header.svelte';
+    import MeetingItem from './UI/MeetingItem.svelte';
+
+    const meetings = [
+        {
+            id: 'm1',
+            title: 'Anti-Racists',
+            subtitle: 'Fight racism together.',
+            description: 'A movement building meeting for people of color, specfially youth, women, queer and trans folks to form community and protect each other.',
+            imgUrl: 'https://images.unsplash.com/photo-1591826246299-9113fd782020?ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
+            COMM: 'To be determined',
+            contactEmail: 'ar@test.com'
+        }, {
+            id: 'm2',
+            title: 'Dog-Friends',
+            subtitle: 'A place for humans with dogs.',
+            description: 'Share tips and other great information about taking care of your dogs.',
+            imgUrl: 'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+            COMM: 'To be determined',
+            contactEmail: 'dog@test.com'
+        }
+    ]
+
 </script>
+
+<style>
+    #meetings {
+        margin-top: 5rem;
+    }
+</style>
 
 <Header />
 
-<h1>The App</h1>
+<section id="meetings">
+    {#each meetings as meeting }
+        <MeetingItem />
 
-<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla temporibus explicabo doloribus facilis perferendis qui aperiam voluptates. Quo sint ipsum rem ut, quis earum modi cum molestiae. In neque exercitationem incidunt. Fugiat quod tempora obcaecati quos illum, commodi, libero similique fuga repellendus accusantium vel eum in sint explicabo veniam adipisci!</h1>
+    {/each}
+</section>
+
